a1145bf662a1fa92b86be6b7374589d9a94040ab
thewoodfish <jasonholt2002@gmail.com>
2023-03-31 19:18:25 +0100

    latest block

--- tester/src/main.rs
+++ tester/src/main.rs
@@ -5,16 +5,18 @@ use std::process::Command;
 fn main() {
     // test functions
     // test_account_creation();
+    // test_hashtable_update();
+
     // test_is_auth();
+    pull_from_ipfs("QmR6bcs7yv7qDK73mvAwXAFqVP2cRi31z4hBddhVuKSrko");
     // test_file_addition();
     // test_getting_sync_info();\
 
-    let real_info = test_get_file_info();
-    let extra_info = test_get_extra_info();
+    // let real_info = test_get_file_info();
+    // let extra_info = test_get_extra_info();
 
-
-    println!("{}", real_info);
-    println!("{}", extra_info);
+    // println!("{}", real_info);
+    // println!("{}", extra_info);
 
     // // println!("success? {}", output.status.success());
     // println!("success? {:#?}", output);
@@ -30,10 +32,32 @@ fn main() {
     // let num: u64 = 1002;
     // let io: Vec<u8> = num.to_ne_bytes().to_vec();
     // println!("{:?}", String::from_utf8(io).unwrap());
+    // println!(
+    //     "{:#?}, {:#?}, {:#?}",
+    //     &str_to_hex("did:sam:root:jhjbsfgiusif78uwn9s980h9"),
+    //     &str_to_hex(&format!("{}", 32323232)),
+    //     &str_to_hex("empty"),
+    // );
 }
 
+// "contract",
+//             "call",
+//             "--contract",
+//             "5EHqUsDvHKTDMT6qsGbagfgGgPRy8YSqtCKkGdexfpt84WdU",
+//             "--message",
+//             "create_new_account",
+//             "--args",
+//             &str_to_hex("did:sam:root:jhjbsfgiusif78uwn9s980h9"),
+//             &str_to_hex(&format!("{}", 32323232)),
+//             &str_to_hex("empty"),
+//             "--suri",
+//             "//Alice",
+//             "--url",
+//             "https://rococo-contracts-rpc.polkadot.io"
+
 // cargo contract call --contract 5E3rCrW49guH5V9HMrBqys76nSgkzneaLvRvupZzT7DEoqj9 --message get_assistant --suri //Alice --dry-run
-// 5GeKCcowiWzVdgnBgchCdreHUbsLSSCmndrxsPdwrTJGK1nH
+// cargo contract call --contract 5Ct5SP6D2viSQJ4HQimdnPCTzSGT71Md9FGGoMo1z3jVjQua  --message create_new_account --args 0x6469643a73616d3a726f6f743a6a686a627366676975736966373875776e39733938306839 0x3332333233323332 0x656d707479 --suri //Alice "blast they annual column pave umbrella again olympic exotic vibrant lemon visa" --url wss://rococo-contracts-rpc.polkadot.io
+// 5Ct5SP6D2viSQJ4HQimdnPCTzSGT71Md9FGGoMo1z3jVjQua
 
 fn test_get_file_info() -> String {
     let did = "did:sam:root:jhjbsfgiusif78uwn9s980h9";
@@ -42,7 +66,7 @@ fn test_get_file_info() -> String {
             "contract",
             "call",
             "--contract",
-            "5GeKCcowiWzVdgnBgchCdreHUbsLSSCmndrxsPdwrTJGK1nH",
+            "5Ct5SP6D2viSQJ4HQimdnPCTzSGT71Md9FGGoMo1z3jVjQua",
             "--message",
             "get_files_info",
             "--args",
@@ -66,7 +90,7 @@ fn test_get_extra_info() -> String {
             "contract",
             "call",
             "--contract",
-            "5GeKCcowiWzVdgnBgchCdreHUbsLSSCmndrxsPdwrTJGK1nH",
+            "5Ct5SP6D2viSQJ4HQimdnPCTzSGT71Md9FGGoMo1z3jVjQua",
             "--message",
             "get_files_extra_info",
             "--args",
@@ -97,7 +121,7 @@ fn test_file_addition() {
             "contract",
             "call",
             "--contract",
-            "5GeKCcowiWzVdgnBgchCdreHUbsLSSCmndrxsPdwrTJGK1nH",
+            "5Ct5SP6D2viSQJ4HQimdnPCTzSGT71Md9FGGoMo1z3jVjQua",
             "--message",
             "update_file_meta",
             "--args",
@@ -124,7 +148,7 @@ fn test_getting_sync_info() {
             "contract",
             "call",
             "--contract",
-            "5GeKCcowiWzVdgnBgchCdreHUbsLSSCmndrxsPdwrTJGK1nH",
+            "5Ct5SP6D2viSQJ4HQimdnPCTzSGT71Md9FGGoMo1z3jVjQua",
             "--message",
             "get_file_sync_info",
             "--args",
@@ -157,27 +181,61 @@ fn test_is_auth() {
             "contract",
             "call",
             "--contract",
-            "5GeKCcowiWzVdgnBgchCdreHUbsLSSCmndrxsPdwrTJGK1nH",
+            "5Ct5SP6D2viSQJ4HQimdnPCTzSGT71Md9FGGoMo1z3jVjQua",
             "--message",
             "account_is_auth",
             "--args",
-            &str_to_hex("did:sam:root:jhjbsfgiusif78uwn9s980h9"),
-            &str_to_hex(&format!("{}", 32323232)),
-            "true",
+            &str_to_hex("did:sam:root:jdlksdfksfuwn9s980h9"),
+            &str_to_hex(&format!("{}", 323232)),
             "--suri",
             "//Alice",
+            // "blast they annual column pave umbrella again olympic exotic vibrant lemon visa",
             "--dry-run",
+            // "--url",
+            // "wss://rococo-contracts-rpc.polkadot.io",
         ])
         .current_dir("../sam_os")
         .output()
         .expect("failed to execute process");
 
     let binding = String::from_utf8_lossy(&output.stdout);
+    println!("{}", binding);
     if parse_contract_boolean(&binding) {
         println!("The account exists!");
     } else {
         println!("The account doesn't exists!")
     }
+
+    println!("{}", parse_contract_return_data(&binding));
+    println!("The nonce is {}", parse_contract_last_u64(&binding));
+}
+
+fn test_hashtable_update() {
+    // update_hashtable(cfg: &Arc<Config>, cid: &str, did: &str, curr_timestamp: u64);
+    let output = Command::new("cargo")
+        .args([
+            "contract",
+            "call",
+            "--contract",
+            "5Ct5SP6D2viSQJ4HQimdnPCTzSGT71Md9FGGoMo1z3jVjQua",
+            "--message",
+            "update_hashtable",
+            "--args",
+            &str_to_hex("dhkjalkhc89ar90rcaroc0kfkajkjf00ad"),
+            &str_to_hex("did:sam:root:jdlksdfksfuwn9s980h9"),
+            &format!("{}", 920099999),
+            "--suri",
+            "//Alice",
+            "--skip-confirm",
+        ])
+        .current_dir("../sam_os")
+        .output()
+        .expect("failed to execute process");
+
+
+    println!("{:#?}", output);
+
+    println!("success? {}", output.status.success())
 }
 
 fn test_account_creation() {
@@ -186,19 +244,27 @@ fn test_account_creation() {
             "contract",
             "call",
             "--contract",
-            "5GeKCcowiWzVdgnBgchCdreHUbsLSSCmndrxsPdwrTJGK1nH",
+            "5Ct5SP6D2viSQJ4HQimdnPCTzSGT71Md9FGGoMo1z3jVjQua",
             "--message",
             "create_new_account",
             "--args",
-            &str_to_hex("did:sam:root:jhjbsfgiusif78uwn9s980h9"),
-            &str_to_hex(&format!("{}", 32323232)),
-            &str_to_hex("empty"),
+            &str_to_hex("did:sam:root:jdlksdfksfuwn9s980h9"),
+            &str_to_hex(&format!("{}", 323232)),
+            &str_to_hex("empcty"),
+            &str_to_hex("hashtable_uri"),
             "--suri",
+            // "blast they annual column pave umbrella again olympic exotic vibrant lemon visa",
+            // "--url",
+            // "wss://rococo-contracts-rpc.polkadot.io",
             "//Alice",
+            // "--dry-run"
+            "--skip-confirm",
         ])
         .current_dir("../sam_os")
         .output()
         .expect("failed to execute process");
+
+    println!("{:#?}", output);
 
     println!("success? {}", output.status.success())
 }
@@ -266,6 +332,17 @@ pub fn parse_contract_tuple_vector(binding: &str) -> String {
     String::from_utf8(parsed).unwrap_or_default()
 }
 
+pub fn parse_contract_last_u64(binding: &str) -> u64 {
+    let chunks = binding.split("UInt(").collect::<Vec<&str>>();
+    let last_chunk: String = chunks[chunks.len() - 1]
+        .to_owned()
+        .as_str()
+        .chars()
+        .filter(|x| x.is_ascii_digit())
+        .collect();
+    last_chunk.parse::<u64>().unwrap_or_default()
+}
+
 // #[ink(message)]
 //         pub fn create_new_account(
 //             &mut self,
@@ -283,3 +360,17 @@ pub fn parse_contract_tuple_vector(binding: &str) -> String {
 //         }
 
 // 5EK4Wuz19DFPSH4bH51r4PZJXwxidE24xgXkUYHQTB9MQQ24  ---->   5HRbmEmnNh25Qb3BZggsVGUCmzDLSfY74TZRsCRzggKbbfiz
+
+// testing IPFS pulling
+fn pull_from_ipfs(cid: &str) -> (bool, String) {
+    let output = Command::new("ipfs")
+        .args(["cat", cid])
+        .output()
+        .expect("failed to read data from node");
+
+    let result = String::from_utf8_lossy(&output.stdout);
+    println!("ipfs file download, cid: {}", cid);
+    println!("result: {}", result);
+
+    (output.status.success(), result.into())
+}
